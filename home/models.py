a169568fc07612abf6550bcb264d3998f08e3103
caio-souza7 <caiostudent7@gmail.com>
2022-03-26 09:44:55 -0300

    Adicionando as classes do ambiente administrativo

--- home/models.py
+++ home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Login(models.Model):
+    usuario = models.charField(max_lenght=15)
+    senha = models.Charfield(max_lenght=30)
+
+    def __strf__(self):
+        return self.usuario
