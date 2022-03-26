fd92b9eec6aaa57e5e621baea414eb42493ebfd0
caio-souza7 <caiostudent7@gmail.com>
2022-03-26 10:54:37 -0300

    make it better

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from home.models import Login
+
+
+@admin.register(Login)
+class detLogin(admin.ModelAdmin):
+    list_display = ('usuario', 'senha')
