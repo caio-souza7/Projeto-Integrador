c635f83b35f37d552de31e8bb849d87bad157105
caio-souza7 <caio.student7@gmail.com>
2022-03-30 19:32:48 -0300

    Feito Atualização no projeto

--- sigma/settings.py
+++ sigma/settings.py
@@ -35,11 +35,11 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'bootstrap5',
     'django.contrib.staticfiles',
     'home',
     'cliente',
     'rest_framework',
-    'bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -126,15 +126,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = 'static'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-STATICFILES_DIR = {
-    os.path.join(BASE_DIR, 'templates/static')
-}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
