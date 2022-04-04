0b14bdadee7680ab967a2a8f667d8f59ac3d8186
caio-souza7 <caio.student7@gmail.com>
2022-04-04 18:17:03 -0300

    Feito atualização no codigo 18:16

--- sigma/urls.py
+++ sigma/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from cliente import views
 from sigma import settings
 
 urlpatterns = [
